9160fa40132f641225ccbde84ce3633ca096dc2e
Damis2 <quadeosaks@gmail.com>
2026-06-11 17:55:58 +0100

    Fix port variable

--- gradmatch-backend/app.py
+++ gradmatch-backend/app.py
@@ -477,4 +477,4 @@ def get_latest_profile(user_id):
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-app.run(debug=False, host='0.0.0.0', port=port) ##
+    app.run(debug=False, host='0.0.0.0', port=port)
